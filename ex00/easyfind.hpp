42ba3c0e98f20f95c5d48592c7fcbf06c48a2759
Abdelali Rami <abrami@e1r2p9.1337.ma>
2026-06-23 08:34:56 +0100

    still need seatch

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,14 +1,18 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <vector>
+// template <typename T>
 template <typename T>
 T easyfind(T a, int b)// if T is container
 {
-    for (int i = 0; i < a.size(); i++)
-    {
-        if (a[i] == b)
-            return a[i]; // pr return i???
-    }
+    // for (int i = 0; i < a.size(); i++)
+    // {
+        // if (a[i] == b)
+        if (find(a.begin(), a.end(), b) != a.end())
+            return find(a.begin(), a.end(), b); // should return an iterator???
+    // }
+    throw ;
 }
 
 #endif
