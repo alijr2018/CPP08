b1079a72d0667a721f704f1abfbc15bacfe51ddc
Abdelali Rami <abrami@e1r4p4.1337.ma>
2026-06-23 21:49:50 +0100

    ex00 done

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -4,14 +4,11 @@
 #include <vector>
 // template <typename T>
 template <typename T>
-T easyfind(T a, int b)// if T is container
+typename T::iterator easyfind(T &a, int b)
 {
-    // for (int i = 0; i < a.size(); i++)
-    // {
-        // if (a[i] == b)
-        if (find(a.begin(), a.end(), b) != a.end())
-            return find(a.begin(), a.end(), b); // should return an iterator???
-    // }
+    typename T::iterator tmp = find(a.begin(), a.end(), b); //search more about this typename
+    if (tmp != a.end())
+        return tmp;
     throw ;
 }
 
