5402bb82b3ccbe4e4bc42f352f3cfd8acca02da8
alijr2018 <alirami120@hotmail.fr>
2026-06-24 08:25:38 +0100

    still main

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,15 +1,34 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
-#include <vector>
+// #include <exception>
+#include <algorithm>
+
+// class ex : public std::exception
+// {
+//     public:
+//         class Not : public std::exception
+//         {
+//             public:
+//                 const char* what() const throw()
+//                 {
+//                     return ("Not found");
+//                 }
+//         };
+// };
+
+
 // template <typename T>
 template <typename T>
 typename T::iterator easyfind(T &a, int b)
 {
-    typename T::iterator tmp = find(a.begin(), a.end(), b); //search more about this typename
+    typename T::iterator tmp = std::find(a.begin(), a.end(), b); //search more about this typename
     if (tmp != a.end())
         return tmp;
-    throw ;
+    // throw ex::Not(); // changed later to a return 
+    return a.end();
 }
 
 #endif
+
+
